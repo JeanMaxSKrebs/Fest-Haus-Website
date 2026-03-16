fd84c98ea5b2495da83787dde9a9d6cdeca996ce
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2026-03-16 05:50:05 -0300

    fix: ajustar cors do backend para cloudflare pages

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://SEU-SITE.pages.dev",
+      "https://https://fest-haus-website.pages.dev/",
     ],
     credentials: true,
   })
